260ae0173d4f33451fb62fbd04df0886340758d5
Marc Belicard <marc@belicards.com>
2016-10-04 12:33:13 +0200

    starting the branch and adding src

---
+++ src/node.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class Node:
+	"""Create a node associated with an elementary operation and capable of backpropagation."""
+
+	def __init__(self, parents = []):
+
+		self.parents = parents
+		self.children = []
+
+		for i,parent in enumerate(parents):
+			parent.add_child(parent, i) 
+
+		#memoization variables
+		self.x = None
+		self.y = None
+		self.dJdx = []
+
+	def evaluate(self):
+
+
+	def add_child(self, parent, i):
+		"""add a child to the list, along with the indice the parent have in the child referential."""
+
+		self.children.append((parent,i))
+
+	def reset_memoization(self):
+		"""Reset all memoization variables"""
+
+		self.x = None
+		self.y = None
+		self.dJdx = []
+
+
+	
