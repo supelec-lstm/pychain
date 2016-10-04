86213446bce126cb5f0cf0e635d0a37b8b9c50cb
yhechaichi <yaqine.hechaichi@gmail.com>
2016-10-04 12:50:48 +0200

    added Graph class

---
+++ src/graph.py
@@ -0,0 +1,26 @@
+
+class Graph:
+    """Create a graph from a set of nodes"""
+    def __init__(self,nodes):
+        pass
+
+    def propagate(self,x):
+        pass
+
+    def backpropagate(self,x,y):
+        pass
+
+    def descend_gradient(self,learning_rate,batch_size):
+        pass
+
+    def batch_descent_gradient(self,learning_rate):
+        pass
+
+    def stochastic_gradient_descent(self,learning_rate):
+        pass
+
+    def reset_memoization(self):
+        pass
+
+    def reset_accumulators(self):
+        pass
