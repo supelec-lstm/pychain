e5aa95cfe134bb2d804bfdd78c50be1ab70b403f
thaisrahoul <thais.rahoul@supelec.fr>
2016-10-04 13:28:47 +0200

    add some subclasses

--- src/node.py
+++ src/node.py
@@ -36,3 +36,54 @@ class Node:
 		self.dJdx = []
 
 
+
+
+class InputNode(Node):
+     """Create the nodes for the inputs of the graph"""
+     
+     def __init__(self, value = None):
+         self.value = value
+         
+     def set_value(self, value):
+         self.value = value
+         
+        
+class LearnableNode(Node):
+    """A node which contains the parameters we want to evaluate"""
+    
+    def __init__(shape, init_function):
+        self.w = 
+        
+    def descend_gradient(learning_rate, batch_size):
+        self.w = self.w - (learning_rate/batch_size)*self.acc_dJdw
+        
+    
+    
+class FunctionNode(Node):
+    """A node which apply an activation function"""
+    
+    def __init__(parent):
+        """parent is a Node"""
+        
+        self.parent = parent
+        
+    def f():
+        """apply the activation function
+        the function is defined in the sub-classes"""
+        
+        raise NotImplementedError
+        
+    def gradient_f():
+        
+        raise NotImplementedError
+        
+
+class SigmoidNode(FunctionNode):
+    
+    def f():
+        return f(self.parent.evaluate())
+        
+    def gradient_f():
+        return f(self.parent.evaluate())*(1-self.parent.evaluate())
+        
+    
